405b52f3d4e80d210786a75ab4d333d765c67c0e
Taner Öksüz <taner.oeksuez@outlook.de>
2024-05-19 07:04:23 +0200

    updated.

--- wolv-z0/benchmark.sh
+++ wolv-z0/benchmark.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -e
 
+export CPU="wolv-z0"
+export XLEN="32"
+export RISCV="/opt/rv32imc"
+export BASEDIR="/home/taner/Project/wolv-benchmark"
+
+export PATH=$PATH:"/opt/rv32imc/bin"
+
 if [ -d "$BASEDIR/$CPU/wolv-benchmark" ]; then
   rm -rf $BASEDIR/$CPU/wolv-benchmark
 fi
 
 mkdir -p $BASEDIR/$CPU/wolv-benchmark
 
+cp -R $BASEDIR/riscv-tests/* $BASEDIR/$CPU/wolv-benchmark/
+
+cp $BASEDIR/$CPU/crt.S $BASEDIR/$CPU/wolv-benchmark/benchmarks/common/
+cp $BASEDIR/$CPU/syscalls.c $BASEDIR/$CPU/wolv-benchmark/benchmarks/common/
+cp $BASEDIR/$CPU/test.ld $BASEDIR/$CPU/wolv-benchmark/benchmarks/common/
+
 cd $BASEDIR/$CPU/wolv-benchmark
 
-cp -R $BASEDIR/riscv-tests/ .
-
-./configure
+./configure --prefix=$RISCV/target
 make -j$(nproc)
