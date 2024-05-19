3d92c94f09db560ddad7087cbfae7f46405a2fbe
Taner Öksüz <taner.oeksuez@outlook.de>
2024-05-19 12:42:12 +0200

    updated.

---
+++ wolv-z7/benchmark.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+export BASEDIR=$(pwd)
+
+export XLEN="32"
+export RISCV_PREFIX="/opt/rv32imfcb/bin/riscv32-unknown-elf-"
+export RISCV_GCC_OPTS="-DPREALLOCATE=1 -static -std=gnu99 -O2 -ffast-math -fno-common -fno-builtin-printf -fno-tree-loop-distribute-patterns"
+
+if [ -d "$BASEDIR/benchmarks" ]; then
+  rm -rf $BASEDIR/benchmarks
+fi
+
+mkdir -p $BASEDIR/benchmarks
+
+cp -r $BASEDIR/../riscv-tests/benchmarks/common $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/dhrystone $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/median $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/memcpy $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/multiply $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/qsort $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/rsort $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/spmv $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/towers $BASEDIR/benchmarks/
+cp -r $BASEDIR/../riscv-tests/benchmarks/Makefile $BASEDIR/benchmarks/
+
+cp -r $BASEDIR/../riscv-tests/env $BASEDIR/
+
+cp $BASEDIR/crt.S $BASEDIR/benchmarks/common/
+cp $BASEDIR/syscalls.c $BASEDIR/benchmarks/common/
+cp $BASEDIR/test.ld $BASEDIR/benchmarks/common/
+
+make -j$(nproc)
---
+++ wolv-z7/riscv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+PREFIX="/opt/rv32imfcb"
+BASEDIR=$(pwd)
+
+
+if [ -d "$PREFIX" ]; then
+  sudo rm -rf $PREFIX
+fi
+
+sudo mkdir -p $PREFIX
+sudo chown -R $USER:$USER $PREFIX
+
+sudo apt-get -y install autoconf automake autotools-dev curl \
+                        python3 python3-pip libmpc-dev libmpfr-dev \
+                        libgmp-dev gawk build-essential bison flex \
+                        texinfo gperf libtool patchutils bc \
+                        zlib1g-dev libexpat-dev ninja-build git \
+                        cmake libglib2.0-dev libslirp-dev
+
+if [ -d "$BASEDIR/riscv-gnu-toolchain" ]; then
+  rm -rf $BASEDIR/riscv-gnu-toolchain
+fi
+
+git clone https://github.com/riscv/riscv-gnu-toolchain $BASEDIR/riscv-gnu-toolchain
+
+cd $BASEDIR/riscv-gnu-toolchain
+
+./configure --prefix=$PREFIX --with-arch=rv32imfc_zba_zbb_zbc_zbs_zicsr --with-abi=ilp32
+make -j$(nproc)
